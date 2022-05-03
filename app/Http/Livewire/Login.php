17c7985481f4f399198662df314cbcdf377343a9
Prokushxx <takinyene@gmail.com>
2022-05-03 10:44:45 -0500

    Complete Library App

---
+++ app/Http/Livewire/Login.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Illuminate\Support\Facades\Auth;
+use Livewire\Component;
+
+class Login extends Component
+{
+  public $email, $password;
+
+  public function login()
+  {
+    $this->validate([
+      'email' => 'required',
+      'password' => 'required',
+    ]);
+
+    if (Auth::attempt(['email' => $this->email, 'password' => $this->password])) {
+      return redirect(url('book'));
+    } else {
+      session()->flash('error', 'Something is wrong');
+    }
+  }
+  
+  public function render()
+  {
+    return view('livewire.login');
+  }
+}
